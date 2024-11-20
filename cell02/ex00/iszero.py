a17643b0072a31af05873ef53fa5e9ce95d79a16
natthapong073 <67030073@kmitl.ac.th>
2024-11-20 09:18:31 +0000

    00

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -2,4 +2,4 @@ number = float(input("Enter a number: "))
 if number == 0:
     print("This number is equal to zero.")
 else:
-    print("This number is different from zero.")0
+    print("This number is different from zero.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+password = "Python is awesome"
+entered_password = input("Enter password: ")
+if entered_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
