1df21b394b5fbd133360853a3281d02f21c4ecaf
natthapong073 <67030073@kmitl.ac.th>
2024-11-20 16:53:50 +0000

    00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+print("Enter a number less than 25")
+number = int(input())
+i = 25 - number
+if i > 0:
+        for i in range(i):
+            i += 1
+            print("Inside the loop, my variable is", number + i)
+else:
+    print("Error")
+    
