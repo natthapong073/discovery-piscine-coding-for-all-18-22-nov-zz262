8bea7c63e1bcf5e7dd18ba41b972f12080723fac
natthapong073 <67030073@kmitl.ac.th>
2024-11-21 09:41:46 +0000

    01

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+for x in array:
+        new_array.append(x + 2)
+print("Original array:", array)
+print("New array:", new_array)
