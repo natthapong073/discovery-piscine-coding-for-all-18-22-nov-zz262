bb4d8337ba8d2dc86020d0a69f304a79d7ce4185
natthapong073 <67030073@kmitl.ac.th>
2024-11-21 04:32:13 +0000

    02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+x = int(input("Give me the first number: "))
+y = int(input("Give me the second number: "))
+print("Thank you!")
+print(x,"+",y,"=",int(x+y))
+print(x,"-",y,"=",int(x-y))
+print(x,"/",y,"=",int(x/y))
+print(x,"*",y,"=",int(x*y))
