dfe0af83cdbbaf5ad290e9f03c1e6e0c9ebcb2ba
natthapong073 <67030073@kmitl.ac.th>
2024-11-22 09:56:24 +0000

    05

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def add_one(number):
+    return number + 1
+
+my_variable = 5
+result = add_one(my_variable)
+
+print("initialize a variable is", my_variable)
+print("after, called the add_one method is", result)
